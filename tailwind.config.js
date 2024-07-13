796ffddd8e5f4daa475c976f1d4650a6d4af40cb
lxnid <dinilr28@outlook.com>
2024-07-13 15:12:56 +0530

    aervcgaerga

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
-        "img-screen-1": `url(/dist/Images/bg-1.png)`,
-      }),
       fontFamily: {
         dahlia: ["dahlia", "sans-serif"],
         prim: ["Inter", "sans-serif"],
