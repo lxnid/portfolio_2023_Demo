a9ae3a4303b377a41ef80a7d15055a9a307d3f72
lxnid <dinilr28@outlook.com>
2024-07-13 14:25:45 +0530

    update bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: (theme) => ({
+        "img-screen-1": `url(Images/bg-1.png)`,
+      }),
       fontFamily: {
         dahlia: ["dahlia", "sans-serif"],
         prim: ["Inter", "sans-serif"],
