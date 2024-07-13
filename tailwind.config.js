f5ba3212af7f2d55283f7c472870446d85b74cd0
lxnid <dinilr28@outlook.com>
2024-07-13 14:21:01 +0530

    add css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,14 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
-        'img-screen-1': `url(Images/bg-1.png)`,
-      }),
       fontFamily: {
-        'dahlia': ['dahlia','sans-serif'],
-        'prim': ['Inter', 'sans-serif'],
-        'sec': ['Poppins', 'arial'],
-        'cursive': ['Ms Madi'],
-        'guest': ['Space Grotesk'],
+        dahlia: ["dahlia", "sans-serif"],
+        prim: ["Inter", "sans-serif"],
+        sec: ["Poppins", "arial"],
+        cursive: ["Ms Madi"],
+        guest: ["Space Grotesk"],
       },
     },
   },
   plugins: [],
-}
+};
